c015dabef9d9c9fba8860ecec164d3220e0cfa3d
Akshit Gaur <akshitgaur@protonmail.com>
2024-09-12 00:47:05 +0530

    Basic Troops

---
+++ scripts/User.gd
@@ -0,0 +1,20 @@
+extends Node3D
+
+const SPEED = 1.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * 0.005)
+		rotate_x(-event.relative.y * 0.005)
+		rotation.x = clamp(rotation.x, -PI / 4, PI / 4)
+		rotation.z = 0
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var input_dir = Input.get_vector("left", "right", "forward", "backward")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	position += direction * SPEED
